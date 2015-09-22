c0c6ccd460735bca67ef97bf9568edccca842622
David Asabina <david@supr.nu>
2015-09-23 00:44:05 +0200

    dumb script for building mesos

---
+++ build-mesos-dns.bash
@@ -0,0 +1,7 @@
+mkdir ~/go
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+go get github.com/tools/godep
+go get github.com/mesosphere/mesos-dns
+cd $GOPATH/src/github.com/mesosphere/mesos-dns
+make restoredeps build
